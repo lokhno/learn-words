a92d4d840c14794154e8bf448839f4a3139e84a6
nlokhno <nlokhno@gmail.com>
2024-02-15 22:22:50 +0300

    add new words

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
 import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Learn Words',
@@ -16,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={roboto.className}>
+      <body>
         <Navbar />
         <main>
           {children}
--- src/data/categoriesData.ts
+++ src/data/categoriesData.ts
@@ -11,6 +11,38 @@ const categoryData: Category[] = [
         id: 3,
         title: "Виды кулирантной обработки продуктов",
     },
+    {
+        id: 4,
+        title: "Units",
+    },
+    {
+        id: 5,
+        title: "Слова из текста про платформу",
+    },
+    {
+        id: 6,
+        title: "Урок christmas",
+    },
+    {
+        id: 7,
+        title: "Видео с ютуба",
+    },
+    {
+        id: 8,
+        title: "Текст про Пати Милса",
+    },
+    {
+        id: 9,
+        title: "adjectives A",
+    },
+    {
+        id: 10,
+        title: "adjectives C",
+    },
+    {
+        id: 11,
+        title: "adjectives B",
+    },
 ]
 
 export default categoryData;
